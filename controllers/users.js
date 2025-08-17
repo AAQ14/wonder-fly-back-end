21ee73c47296a57cf133ce0ab74e83558c1502fd
AMAL <hopealasfoorgmail.com>
2025-08-17 21:15:11 +0300

    notes

--- controllers/users.js
+++ controllers/users.js
@@ -55,3 +55,91 @@ exports.login = async (req, res) => {
     res.status(500).json({ message: 'Server error' })
   }
 }
+
+
+
+// //Const👇
+// const User = require('../models/User')
+
+// //Create👇
+// async function createUser(req, res) {
+//     try {
+//         req.body.userType = "user"
+//         const createdUser = await User.create(req.body)
+//         res.status(201).json(createdUser)
+//     } catch (error) {
+//         console.log(error)
+//         res.status(500).json({ error: error.message })
+//     }
+// }
+
+
+// //Delete👇
+// async function deleteUser(req, res) {
+//     try {
+//         const user = await User.findByIdAndDelete(req.params.id)
+//         if (user) {
+//             res.status(200).json(user)
+//         } else {
+//             res.sendStatus(404)
+//         }
+//     }
+//     catch {
+//         console.log(error)
+//         res.status(500).json({ error: error.message })
+//     }
+// }
+
+// //get all users👇
+// const indexUser = async (req, res) => {
+//     try {
+//         const users = await User.find()
+//         if (users.length) {
+//             res.status(200).json(users)
+//         } else {
+//             res.sendStatus(404)
+//         }
+//     } catch (err) {
+//         console.log(err)
+//         res.status(500).json({error : err.message})
+//     }
+// }
+
+// //get user details👇
+// const userDetails = async (req,res) =>{
+//     try {
+//         const user = await User.findById(req.params.id)
+//         if(user){
+//             res.status(200).json(user)
+//         }else{
+//             res.sendStatus(404)
+//         }
+//     } catch (err) {
+//         console.log(err)
+//         res.status(500)
+//     }
+// }
+
+// const updateUser = async (req,res) =>{
+// try {
+//     const user = await User.findByIdAndUpdate(req.params.id, req.body)
+//     if(user){
+//         res.status(200).json(user)
+//     }else{
+//         res.sendStatus(404)
+//     }
+// } catch (err) {
+//     console.log(err)
+//     res.status(500).json({error: err})
+// }
+// }
+
+
+// //Export👇
+// module.exports = {
+//     createUser,
+//     deleteUser,
+//     indexUser, 
+//     userDetails,
+//     updateUser
+// }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,3 +6,14 @@ router.post('/signup', authController.register)
 router.post('/login', authController.login)
 
 module.exports = router
+
+// const router = require('express').Router()
+// const userController = require('../controllers/users')
+
+// router.get("/", userController.indexUser)
+// router.post("/create", userController.createUser)
+// router.get("/:id", userController.userDetails)
+// router.put("/update/:id", userController.updateUser)
+// router.delete("/delete/:id", userController.deleteUser)
+
+// module.exports = router
