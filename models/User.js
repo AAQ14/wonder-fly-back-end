da0ded2b64af807dd43cb8a3dee351d2f9492198
AAQ14 <80754496+AAQ14@users.noreply.github.com>
2025-08-17 11:07:13 +0300

    editing user model

--- models/User.js
+++ models/User.js
@@ -1,24 +1,24 @@
-const {model, Schema} = require('mongoose')
+const { model, Schema } = require('mongoose')
 
 const userSchema = new Schema({
     firstName: {
         type: String,
-        required : true
+        required: true
     },
     lastName: {
         type: String,
-        required : true
-    }, 
+        required: true
+    },
     email: {
         type: String,
         required: true,
-        unique:true,
-        lowercase:true
-    }, userType :{
+        unique: true,
+        lowercase: true
+    }, userType: {
         type: String,
-        required: true,
-        enum: ["user", "manager"]
-    },bookedFlights :{
+        enum: ["user", "manager"],
+        default: "user"
+    }, bookedFlights: {
         type: Schema.Types.ObjectId,
         ref: 'Flight'
     }
