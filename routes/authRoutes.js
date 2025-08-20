e83010433d72b8d652684b96114afa374207167c
AAQ14 <80754496+AAQ14@users.noreply.github.com>
2025-08-21 02:44:09 +0300

    send otp

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,14 @@
 const router = require('express').Router()
 const authController = require('../controllers/auth')
 const express = require('express')
+const userAuth = require('../middleware/userAuth')
 
 router.post('/signup', authController.register)
 router.post('/login', authController.login)
 router.post('/logout', authController.logout)
+router.post('/send-verify-otp', userAuth, authController.sendVerifyOtp)
+router.post('/verify-account', userAuth, authController.verifyEmail)
+
 
 
 module.exports = router
